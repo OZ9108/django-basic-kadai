9a135c52d6d6720ef64a52e466e18ce13fb8afbb
OZ9108 <omk9108@outlook.jp>
2025-07-19 11:03:00 +0900

    課題作成

---
+++ javascript-basic/kadai_020/event.js
@@ -0,0 +1,6 @@
+const btnClick=document.getElementById('btn');
+
+btnClick.addEventListener('click',()=>{
+    const textchange=document.getElementById('text');
+    textchange.textContent='ボタンをクリックしました';
+})
