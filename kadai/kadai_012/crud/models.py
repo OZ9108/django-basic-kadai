5c0b9c07271a2ff6c1e7f53532ae32c213516627
OZ9108 <omk9108@outlook.jp>
2025-08-18 13:08:29 +0900

    課題修正

--- kadai/kadai_012/crud/models.py
+++ kadai/kadai_012/crud/models.py
@@ -13,6 +13,7 @@ class Product(models.Model):
     price=models.PositiveIntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     img=models.ImageField(blank=True, default='noImage.png')
+    detail=models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
